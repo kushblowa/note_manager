3f80990f7b10f2784cd0124560e79e2189ce776e
kushblowa <igor.varlakhov@gmail.com>
2024-12-27 20:01:18 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'kushblowa'
+title = 'Jugg'
+content = 'Do not forget to jugg'
+status = 'omw'
+created_date = '18-12-2024'
+issue_date = '25-12-2024'
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дата истечения:", issue_date)
+
